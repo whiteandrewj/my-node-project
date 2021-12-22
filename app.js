15e355a5f1fb80b04f17838b72b11504779864ba
Andrew White <ajwhite@AJWhite8F2JPN2.pclc0.merkle.local>
2021-12-22 17:04:57 -0600

    add server.io module

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-var path = require('path')
+var path = require('path');
+var http = require('http');
 var express = require('express');
+var socketio = require('socket.io');
+
 var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
+server.listen(3000);
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.post('/submit-student-data', (req, res) => {
-	var name = req.body.firstName + ' ' + req.body.lastName;
-	res.send(name + ' Submitted Successfully!');
+	res.send('POST Request');
 });
 
 app.put('/update-data', (req, res) => res.send('PUT Request'));
 
 app.delete('/delete-data', (req, res) => res.send('DELETE Request'));
 
-
-var server = app.listen(3000, () => {
-	console.log('Node server is running...');
-
-});
