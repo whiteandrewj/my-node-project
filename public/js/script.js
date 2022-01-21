c71d91cb3b3dec1c3b393eeee4fb9595c40de99d
Andrew White <ajwhite@AJWhite8F2JPN2.pclc0.merkle.local>
2022-01-21 16:47:11 -0600

    work on client js

--- public/js/script.js
+++ public/js/script.js
@@ -1,95 +1,123 @@
 const gameState = {
-    playerTurn: "O",
+    playerTurn: "X",
     gameBoard: {
-        a1: null,
-        b1: null,
-        c1: null,
-        a2: null,
-        b2: null,
-        c2: null,
-        a3: null,
-        b3: null,
-        c3: null
+        a1: "",
+        b1: "",
+        c1: "",
+        a2: "",
+        b2: "",
+        c2: "",
+        a3: "",
+        b3: "",
+        c3: ""
     }
 }
 
 var a1 = document.getElementById("a1").addEventListener("click", e => {
-    Object.defineProperty(gameState.gameBoard,"a1",{value : "X"});
-    updateGamestate();
+    gameState.gameBoard["a1"] = gameState.playerTurn;
+    updateGameState();
 })
 var b1 = document.getElementById("b1").addEventListener("click", e => {
-    gameState.gameBoard["b1"] = "X";
-    updateGamestate();
+    gameState.gameBoard["b1"] = gameState.playerTurn;
+    updateGameState();
 })
 var c1 = document.getElementById("c1").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["c1"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["c1"] = gameState.playerTurn;
+    updateGameState();
 })
 
 var a2 = document.getElementById("a2").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["a2"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["a2"] = gameState.playerTurn;
+    updateGameState();
 })
 var b2 = document.getElementById("b2").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["b2"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["b2"] = gameState.playerTurn;
+    updateGameState();
 })
 var c2 = document.getElementById("c2").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["c2"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["c2"] = gameState.playerTurn;
+    updateGameState();
 })
 
 var a3 = document.getElementById("a3").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["a3"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["a3"] = gameState.playerTurn;
+    updateGameState();
 })
 var b3 = document.getElementById("b3").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["b3"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["b3"] = gameState.playerTurn;
+    updateGameState();
 })
 var c3 = document.getElementById("c3").addEventListener("click", e => {
-    Object.defineProperties(gameState.gameBoard["c3"]) = "X"
-    updateGamestate();
+    gameState.gameBoard["c3"] = gameState.playerTurn;
+    updateGameState();
 })
-/*
-a1.addEventListener("click", e => {
-    a1.setAttribute("class", "btn btn-success")
-    a1.setAttribute("value", "X")
-    a1.disabled = true
-})
-b1.addEventListener("click", e => {
-    b1.setAttribute("class", "btn btn-success")
-    b1.setAttribute("value", "X")
-    b1.disabled = true
-})
-c1.addEventListener("click", e => {
-    c1.setAttribute("class", "btn btn-success")
-    c1.setAttribute("value", "X")
-    c1.disabled = true
-})
-*/
 
-function updateGamestate() {
+
+
+function updateGameState() {
+    
+    var isGameOver = checkForWinner();
+
+    if (isGameOver == false) {
+        switchPlayerTurn();
+    }
+
     //iterate over gameBoard object
     for (var gameSpaceId of Object.keys(gameState.gameBoard)) {
 
-        console.log(gameSpaceId)
         //get html element
-        var boardSquare = document.getElementById(gameSpaceId)
+        var htmlElementId = document.getElementById(gameSpaceId)
 
         //set element attributes
         if (gameState.gameBoard[gameSpaceId] == "X") {
-            boardSquare.setAttribute("class", "btn btn-primary")
-            boardSquare.setAttribute("value", "X")
-            boardSquare.disabled = true
+            htmlElementId.setAttribute("class", "btn btn-primary")
+            htmlElementId.setAttribute("value", "X")
+            htmlElementId.disabled = true
         } else if (gameState.gameBoard[gameSpaceId] == "O") {
-            boardSquare.setAttribute("class", "btn btn-danger")
-            boardSquare.setAttribute("value", "O")
-            boardSquare.disabled = true
+            htmlElementId.setAttribute("class", "btn btn-danger")
+            htmlElementId.setAttribute("value", "O")
+            htmlElementId.disabled = true
         } else {
-            boardSquare.setAttribute("class", "btn btn-outline-primary")
-            boardSquare.setAttribute("value", null)
-            boardSquare.disabled = false 
+            if (isGameOver) {
+                htmlElementId.setAttribute("class", "btn btn-secondary")
+                htmlElementId.disabled = true;
+            } else {
+                if (gameState.playerTurn == "X") {
+                htmlElementId.setAttribute("class", "btn btn-outline-primary")
+                }
+                if (gameState.playerTurn == "O") {
+                htmlElementId.setAttribute("class", "btn btn-outline-danger")
+                }
+                htmlElementId.setAttribute("value", "")
+                htmlElementId.disabled = false 
+            }
         }
+    }
+    console.log(gameState);
+}
+
+function checkForWinner() {
+    if ((gameState.gameBoard.a1 == gameState.playerTurn && gameState.gameBoard.a2 == gameState.playerTurn && gameState.gameBoard.a3 == gameState.playerTurn)
+    || (gameState.gameBoard.b1 == gameState.playerTurn && gameState.gameBoard.b2 == gameState.playerTurn && gameState.gameBoard.b3 == gameState.playerTurn)
+    || (gameState.gameBoard.c1 == gameState.playerTurn && gameState.gameBoard.c2 == gameState.playerTurn && gameState.gameBoard.c3 == gameState.playerTurn)
+    || (gameState.gameBoard.a1 == gameState.playerTurn && gameState.gameBoard.b1 == gameState.playerTurn && gameState.gameBoard.c1 == gameState.playerTurn)
+    || (gameState.gameBoard.a2 == gameState.playerTurn && gameState.gameBoard.b2 == gameState.playerTurn && gameState.gameBoard.c2 == gameState.playerTurn)
+    || (gameState.gameBoard.a3 == gameState.playerTurn && gameState.gameBoard.b3 == gameState.playerTurn && gameState.gameBoard.c3 == gameState.playerTurn)
+    || (gameState.gameBoard.a1 == gameState.playerTurn && gameState.gameBoard.b2 == gameState.playerTurn && gameState.gameBoard.c3 == gameState.playerTurn)
+    || (gameState.gameBoard.a3 == gameState.playerTurn && gameState.gameBoard.b2 == gameState.playerTurn && gameState.gameBoard.c1 == gameState.playerTurn)
+    ) {
+        console.log(gameState.playerTurn + " is the winner!");
+        return true;
+    } else {
+        console.log("no winner yet");
+        return false;
+    }
+}
+
+function switchPlayerTurn() {
+    if (gameState.playerTurn == "X") {
+        gameState.playerTurn = "O";
+    } else if (gameState.playerTurn == "O") {
+        gameState.playerTurn = "X";
     }
 }
